83c81166118afc024127953f3636f150d333ffa2
Rithika <rvarati@gmail.com>
2013-10-31 22:40:48 -0500

    changes to test game 4

--- src/gamePlayEngine/GamePlayEngine.java
+++ src/gamePlayEngine/GamePlayEngine.java
@@ -31,13 +31,14 @@ public class GamePlayEngine {
 		gameModelBoundary.setView(gameView);		
 		//start the game by calling modelboundary
 		//gameModelBoundary.startGame("./GameXML/TestGame1_Variations/BackGround-Color-ChangeV1.2.xml");
+		//gameModelBoundary.startGame("./GameXML/TestGame1_Variations/Game1-Final.xml");
 
 		//gameModelBoundary.startGame("./GameXML/TestGame2_Variations/Button-Color-ChangeV1.2.xml");
 
 		//gameModelBoundary.startGame("./GameXML/TestGame2_Variations/Button-Color-ChangeV1.2.xml");
-
+		//gameModelBoundary.startGame("./GameXML/TestGame3_Variations/BackGround-Color-ChangeV1.3.xml");
 		//gameModelBoundary.startGame("./GameXML/TestGame3_Variations/TestGame3_new_10.xml");
-		gameModelBoundary.startGame("./GameXML/TestGame4_Variations/TestGame_4_5.xml");
+		gameModelBoundary.startGame("./GameXML/TestGame4_Variations/TestGame4-Final.xml");
 		//gameModelBoundary.startGame("./GameXML/TestGame3_Variations/BackGround-Color-ChangeV1.3.xml");
 		//gameModelBoundary.startGame("./GameXML/TestGame5_Variations/TestGame5_new_1.xml");
 		
